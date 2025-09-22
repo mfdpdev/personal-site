d3a3bd2e41ed040884550c416c19027bdcb5bdfb
lord <danipramoedya2002@gmail.com>
2025-09-22 13:31:41 +0700

    style: change style projects and certiface component

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -2,15 +2,36 @@ export default function Certificate(){
   return (
     <>
       <div className="mt-12 text-slate-300 text-center">
-        <h3 className="text-2xl font-bold text-red-500">Low Code Showcase</h3>
-        <h1 className="text-3xl font-bold mt-4">Low Code Website Development</h1>
-        <p className="text-base">Simple designs can also be developed nicely even only with a CMS. Feel free to look at some of my works</p>
+        <h3 className="text-2xl font-bold text-red-500">Certificates</h3>
+        <h1 className="text-4xl font-bold mt-4">Verified Skills & Expertise</h1>
+        <p className="text-base">A collection of certifications that showcase my knowlage in web, mobile development, and IT</p>
         <div className="mt-8 gap-5 grid grid grid-cols-1 lg:grid-cols-2">
-          <div className="text-start p-4 bg-slate-800 bg-opacity-80 rounded-xl border border-slate-700">
-            <div className="mb-4 rounded-xl overflow-hidden bg-slate-300 h-42">
+          <div className="relative bottom-0 hover:bottom-2 transition-all duration-700  text-start p-4 bg-slate-800 bg-opacity-80 rounded-xl border border-slate-700">
+            <div className="">
+              <div className="flex justify-between mb-2">
+                <h1 className="text-lg font-bold">
+                  Belajar Backend Javascript
+                </h1>
+                <div>
+                  2024
+                </div>
+              </div>
+              <div>
+                <div className="flex gap-4">
+                  icon
+                  <p>Dicoding</p>
+                </div>
+                <div className="flex gap-4">
+                  icon
+                  <p>2022 - 2025</p>
+                </div>
+              </div>
+              <div className="hover:bg-red-400 hover:animate-bounceUpDown rounded-lg mt-6 w-fit px-6 py-2 bg-red-500">
+                <button>
+                  Certificate
+                </button>
+              </div>
             </div>
-            <h1 className="text-xl font-bold">Batavia Jokers</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis, justo eget fermentum gravida, odio luctus vel. Sed fringilla justo sit amet augue facilisis, nec auctor eros condimentum.</p>
           </div>
         </div>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,24 +1,43 @@
 export default function Projects(){
+              // <div className="hover:bg-red-400 hover:animate-bounceUpDown rounded-lg mt-6 w-fit px-6 py-4 bg-red-500 text-lg font-bold">
+              //   <button>
+              //     Visit Website
+              //   </button>
+              // </div>
+        // <div className="mt-12 gap-5 grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
   return (
     <>
-      <div className="mt-60 text-slate-300 text-center">
-        <h3 className="text-2xl font-bold text-red-500">Low Code Showcase</h3>
-        <h1 className="text-3xl font-bold mt-4">Low Code Website Development</h1>
-        <p className="text-base">Simple designs can also be developed nicely even only with a CMS. Feel free to look at some of my works</p>
-        <div className="mt-8 gap-5 grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          <div className="relative bottom-0 hover:bottom-2 transition-all duration-300 overflow-hidden bg-slate-800 bg-opacity-80 rounded-xl border border-slate-700">
-            <div className="bg-white h-42">
+      <div className="mt-60 text-slate-300">
+        <div className="w-2/3 mx-auto">
+          <h3 className="text-2xl font-bold text-red-500 text-center">Projects Showcase</h3>
+          <h1 className="text-4xl font-bold mt-4 text-center">Innovative Web & Application Development Solutions</h1>
+        </div>
+        <div className="mt-12 gap-8 grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <a href="" className="relative bottom-0 hover:bottom-2 transition-all duration-700 overflow-hidden bg-slate-800 bg-opacity-80 rounded-xl border border-slate-700">
+            <div className="bg-slate-300 h-52">
             </div>
-            <div className="my-10 flex flex-col items-center justify-center">
-              <h1 className="text-xl font-bold">Batavia Jokers</h1>
-              <p>community website</p>
-              <div className="hover:bg-red-400 hover:animate-bounceUpDown rounded-lg mt-6 w-fit px-6 py-4 bg-red-500 text-lg font-bold">
-                <button>
-                  Visit Website
-                </button>
-              </div>
+            <div className="p-4">
+              <h1 className="text-xl font-bold">BE - Contact Management</h1>
+              <p className="text-md">Project RESTful API using Typescript</p>
+              <ul className="mt-4 text-sm list-disc ml-4">
+                <li>
+                  CRUD Operations
+                </li>
+                <li>
+                  Validation with Zod
+                </li>
+                <li>
+                  Authentication support
+                </li>
+                <li>
+                  Environtment-based configuration
+                </li>
+                <li>
+                  Error handling
+                </li>
+              </ul>
             </div>
-          </div>
+          </a>
         </div>
       </div>
     </>
