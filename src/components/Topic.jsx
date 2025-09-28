013aad24c06a7820666a5bba152032e4672999f8
lord <danipramoedya2002@gmail.com>
2025-09-28 20:13:20 +0700

    fix: Fix overflow on the topic section

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -13,16 +13,18 @@ export default function Topic(){
 
   return (
     <>
-      <div className="flex flex-wrap text-slate-300 mt-12 gap-y-6 justify-between max-w-3xl mx-auto">
-        <div ref={titleRef} className={`grid gap-y-2 md:w-4/6 transform transition duration-[1s] ${titleInView ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"}`}>
-          <div>
-            <p>Let's have conversation in :</p>
-            <div className="flex flex-wrap gap-4 my-6">
-              <div className="w-fit px-5 py-3 rounded-xl border border-slate-700">Bahasa Indonesia</div>
-              <div className="w-fit px-5 py-3 rounded-xl border border-slate-700">Intermediate English</div>
+      <div className="flex flex-wrap text-slate-300 mt-12 gap-y-6 justify-between max-w-3xl mx-auto overflow-hidden">
+        <div ref={titleRef} className={`md:w-4/6`}>
+          <div className={`grid gap-y-2 w-full transform transition duration-[1s] ${titleInView ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"}`}>
+            <div>
+              <p>Let's have conversation in :</p>
+              <div className="flex flex-wrap gap-4 my-6">
+                <div className="w-fit px-5 py-3 rounded-xl border border-slate-700">Bahasa Indonesia</div>
+                <div className="w-fit px-5 py-3 rounded-xl border border-slate-700">Intermediate English</div>
+              </div>
             </div>
+            <h1 className="md:order-first text-3xl font-bold">Here's five topics that I can talk about, for 15 minutes with zero preparation:</h1>
           </div>
-          <h1 className="md:order-first text-3xl font-bold">Here's five topics that I can talk about, for 15 minutes with zero preparation:</h1>
         </div>
         <div ref={topicRef} className={`transform transition duration-[1s] ${topicInView ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}>
           <ol>
